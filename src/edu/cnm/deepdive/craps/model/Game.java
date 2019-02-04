cf3d19815ac9c10b0272f424fb6ed642b2e45e86
Felipe Cook <felipepcook@gmail.com>
2019-02-04 14:53:24 -0700

    Added Javadoc comments and generated HTML.

--- src/edu/cnm/deepdive/craps/model/Game.java
+++ src/edu/cnm/deepdive/craps/model/Game.java
@@ -1,12 +1,20 @@
 package edu.cnm.deepdive.craps.model;
 
 
-import com.sun.tools.hat.internal.util.ArraySorter;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * <code>Game</code> implements a simple play engine for the main roll/bet of a game of Craps.
+ * Additionally it can be used to keep tallies across several rounds of play. Side bets are not
+ * supported.
+ *
+ * @author Felipe Cook &amp; Nicholas Bennett
+ * @version 1.0
+ *
+ */
 public class Game {
 
   private int pointValue;
@@ -16,11 +24,21 @@ public class Game {
   private long wins;
   private long losses;
 
+  /**
+   *Initializes this instance with the specified source of randomness.
+   * @param rng source of randomness.
+   */
   public Game(Random rng) {
     this.rng = rng;
     rolls = new LinkedList<>();
   }
 
+  /**
+   * Prepares this instance to play a round&mdash;i.e. returns the game state
+   * to {@link State#COME_OUT}, clears the established point, and clears the list of rolls.
+   * This method <strong>must</strong> be invoked prior to {@link #play()}, even
+   * immediately after creating and initializing the instance via {@link #Game(Random)}.
+   */
   public void reset() {
     state = State.COME_OUT;
     pointValue = 0;
@@ -40,6 +58,12 @@ public class Game {
     rolls.add(diceRoll);
   }
 
+  /**
+   * Plays one round of Craps, from come-out roll to win or loss. If this is
+   * invoked on an instance already in a terminal state{@link State#WIN} or {@link State#LOSS}, no
+   * exception is thrown, but the state of this <code>Game</code> instance doesnt change.
+   * @return terminal state ({@link State#WIN} or {@link State#LOSS} of game)
+   */
   public State play() {
     while (state != State.WIN && state != State.LOSS) {
       roll();
@@ -52,14 +76,31 @@ public class Game {
     return state;
   }
 
+  /**
+   * Returns established point.
+   *
+   * @return
+   */
   public int getPointValue() {
     return pointValue;
   }
 
+  /**
+   * Returns current state of game. Tis will be the {@link State#COME_OUT} state after
+   * {@link #reset()} is invoked, and a terminal state ({@link State#WIN} or
+   * {@link State#LOSS} after {@link #play()} is invoked.
+   * @return
+   */
   public State getState() {
     return state;
   }
 
+  /**
+   * Constructs and returns a deep (safe) copy of the list of rolls in the
+   * current game.
+   *
+   * @return
+   */
   public List<int[]> getRolls() {
     List<int[]> copy = new LinkedList<>();
     for (int[] roll : rolls) {
@@ -68,10 +109,20 @@ public class Game {
     return copy;
   }
 
+  /**
+   * Returns the tally of wins recorded since this instance was created and
+   * initialized.
+   * @return
+   */
   public long getWins() {
     return wins;
   }
 
+  /**
+   * Returns the tally of losses recorded since this instance was created and
+   * initialized.
+   * @return
+   */
   public long getLosses() {
     return losses;
   }
